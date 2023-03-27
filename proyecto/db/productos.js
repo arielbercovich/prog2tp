d2d61fd7511886bd32564386496e0c58224f43bc
Manuelzimmermann <zimmermannm@udesa.edu.ar>
2023-03-27 12:19:34 -0300

    boludeces

--- proyecto/db/productos.js
+++ proyecto/db/productos.js
@@ -2,13 +2,11 @@ let productos = {
    lista:[
     {
     nombre: 'Camiseta Boca:',
-
     descripcion: 'Esta es una camiseta con olor a caca',
     comentarios: "camiseta titular"
     },
     {
     nombre: 'Camiseta River:',
-
     descripcion: 'Esta es una camiseta con olor a perfume',
     comentarios: "camiseta titular"
     },
