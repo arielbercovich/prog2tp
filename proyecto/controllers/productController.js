dbcd9ed2ac7e88032fbb8097ddcda13f238c832a
Manuelzimmermann <zimmermannm@udesa.edu.ar>
2023-03-31 18:15:03 -0300

    linkeado editar

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -3,6 +3,9 @@ let db = require('../db/productos');
 let productController = {
     index: function(req, res){
         return res.render('product')
+    },
+    add: function (req, res){
+        return res.render('product-add')
     }
 };
 
--- proyecto/routes/productos.js
+++ proyecto/routes/productos.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let productController = require('../controllers/productController.js');
 
 router.get('/', productController.index);
+router.get('/add', productController.add);
 
 
 module.exports = router;
