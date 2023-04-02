176e67f06716686bfb869bbcd764e065e9e4bbab
arielbercovich <abercovich@udesa.edu.ar>
2023-04-02 18:48:12 -0300

    quilombo

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -2,7 +2,7 @@ let db = require('../db/productos.js');
 
 let productController = {
     index: function(req, res){
-        return res.render('product', {productos:db.productos})
+        return res.render('product', {productos: db.lista})
     },
     add: function (req, res){
         return res.render('product-add')
