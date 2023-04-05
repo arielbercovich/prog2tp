c771f11a6c2f6d54ae14c2d378e05e0ab267ed19
arielbercovich <abercovich@udesa.edu.ar>
2023-04-05 14:03:36 -0300

    f

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -3,13 +3,13 @@ let db = require('../db/productos.js');
 let productController = {
 
     index: function(req, res){
-        return res.render('index', {productos: db.lista})
+        return res.render('index', {arqueros: db.arqueros, volantes: db.volantes, delanteros: db.delanteros})
     },
     add: function (req, res){
         return res.render('product-add')
     },
     show: function (req, res){
-        return res.render('product', {producto: db.lista})
+        return res.render('product', {producto: db.delanteros})
         
     }
 };
--- proyecto/db/productos.js
+++ proyecto/db/productos.js
@@ -1,30 +1,195 @@
 let productos = {
-   lista: [
+   arqueros: [
+        // {
+        //     id:1,
+        //     nombre: 'Darío Benedetto',
+        //     descripcion: 'Darío Ismael "Pipa" Benedetto es un futbolista argentino que juega de Delantero como número 9. Actualmente es propiedad del Club Atlético Boca Juniors de la Liga Profesional de Fútbol Argentino.',
+        //     imagen: "benedetto_84f59.jpg",
+        //     detalle: 'Boca Juniors - Delantero Centro - 17/05/1990 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Sebastián Battaglia",
+        //             texto:"Excelente jugador el Pipa! Muchisima experiencia y potrero. Lo recomiendo para cualquier club, gran 9",
+        //         },
+        //        {
+        //             id: 2,
+        //             usuario: "Gustavo Alfaro",
+        //             texto:"Un gran referente de plantel y excelente profesional, conmigo siempre se portó de maravilla y respeto" 
+        //         }, 
+        //         {
+        //             id: 3,
+        //             usuario: "Guillermo Barros Schelotto",
+        //             texto:"Tiene un olfato goleador marcado, cuando lo dirigí en 2017 me hizo acordar mucho al mejor Palermo. Es el nuevo ídolo de Boca" 
+        //         }
+
+        //     ]
+        // },
+        // {
+        //     id:2,
+        //     nombre: 'Baltasar Barcia',
+        //     descripcion: 'Mateo Baltasar Barcia Fernández es un volante Uruguayo nacido el 19/02/2001 en Pando (URU). Actualmente juega en Independiente de Avellaneda. Se desenvuelve como mediocampista ofensivo, disputó 7 partidos y convirtió 1 goles en Argentina (Primera División), mientras que jugó 34 partidos y convirtió 2 goles en el extranjero',
+        //     imagen: "Baltazar barcia.jpg",
+        //     detalle: 'Independiente - Volante Ofensivo - 19/02/2001 - URU',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Mario Saralegui",
+        //             texto:"Jugador con mucho potencial, lo tuve en 5ta división y sus condiciones como clásico 10 se notaban",
+        //         },
+        //     ]
+        // },
+        
+        // {
+        //     id:3,
+        //     nombre: 'Ignacio Fernández',
+        //     descripcion: 'Ignacio Martín Fernández es un futbolista argentino. Se desempeña como mediocampista y su equipo actual es River Plate de la Primera División de Argentina',
+        //     imagen: "nacho fernandez.png",
+        //     detalle: 'River Plate - Mediocampista Ofensivo - 12/01/1990 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Gerardo",
+        //             texto:"Jugador Historico en River. Estuvo en el plantel de la final en madrid 2018",
+        //         },
+        //     ]
+        // },
+
+        // {
+        //     id:4,
+        //     nombre: 'Lisandro Lopez',
+        //     descripcion: 'Lisandro López es un futbolista argentino que se desempeña como delantero en el Club Atlético Sarmiento de la Primera División de Argentina.',
+        //     imagen: "lisandro lopez.jpg",
+        //     detalle: 'Sarmiento - Delantero Centro - 02/03/1983 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Julian",
+        //             texto:"Idolo en Racing. Las gano todas",
+        //         },
+        //     ]
+        // },
+
+        // {
+        //     id:5,
+        //     nombre: 'Vicente Taborda',
+        //     descripcion: 'Lisandro López es un futbolista] argentino que se desempeña como delantero en el Club Atlético Sarmiento de la Primera División de Argentina.',
+        //     imagen: "taborda.jpeg",
+        //     detalle: 'Platense - Volante Ofensivo - 22/05/2001 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Julian",
+        //             texto:"JUgador joven crack",
+        //         },
+        //     ]
+        // },
+
+        
         {
-            id:1,
-            nombre: 'Darío Benedetto',
-            descripcion: 'Darío Ismael "Pipa" Benedetto es un futbolista argentino que juega de Delantero como número 9. Actualmente es propiedad del Club Atlético Boca Juniors de la Liga Profesional de Fútbol Argentino.',
-            imagen: "benedetto_84f59.jpg",
-            detalle: 'Boca Juniors - Delantero Centro - 17/05/1990 - ARG',
+            id:6,
+            nombre: 'Gabriel Arias',
+            descripcion: 'Gabriel Arias Arroyo es un futbolista chileno nacido en Argentina. Juega como arquero en Racing Club de la Primera División de Argentina y es internacional con la selección de Chile.',
+            imagen: "880_arias.jpg",
+            detalle: 'Racing - Arquero - 02/03/1990 - CHI',
             comentarios: [
                 {
                     id: 1,
-                    usuario: "Sebastián Battaglia",
-                    texto:"Excelente jugador el Pipa! Muchisima experiencia y potrero. Lo recomiendo para cualquier club, gran 9",
+                    usuario: "Julian",
+                    texto:"Idolo en Racing. Las gano todas",
                 },
-               {
-                    id: 2,
-                    usuario: "Gustavo Alfaro",
-                    texto:"Un gran referente de plantel y excelente profesional, conmigo siempre se portó de maravilla y respeto" 
-                }, 
-                {
-                    id: 3,
-                    usuario: "Guillermo Barros Schelotto",
-                    texto:"Tiene un olfato goleador marcado, cuando lo dirigí en 2017 me hizo acordar mucho al mejor Palermo. Es el nuevo ídolo de Boca" 
-                }
-
             ]
         },
+
+        // {
+        //     id:7,
+        //     nombre: 'Mateo Retegui',
+        //     descripcion: 'Mateo Retegui es un futbolista profesional que juega como centrodelantero en el Club Atlético Tigre de la Liga Profesional argentina. Nacido y criado en Argentina de padres argentinos, siendo su abuelo materno italiano, es internacional absoluto con la selección de Italia.',
+        //     imagen: "retegui.jpeg",
+        //     detalle: 'Tigre - Delantero Centro - 07/10/2000 - ITA',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Julian",
+        //             texto:"Hincha de tigre. Nos dio muchas alegrias",
+        //         },
+        //     ]
+        // },
+
+        // {
+        //     id:8,
+        //     nombre: 'José Sand',
+        //     descripcion: 'José Gustavo Sand ​es un futbolista profesional argentino que se desempeña como delantero en el Club Atlético Lanús, de la Primera División de Argentina. Es el máximo goleador en la historia de Lanús, con 170 goles y uno de sus ídolos históricos',
+        //     imagen: "pepe sand.jpg",
+        //     detalle: 'Lanus - Delantero Centro - 02/03/1980 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Julian",
+        //             texto:"Maximo goleador historico",
+        //         },
+        //     ]
+        // },
+        // {
+        //     id:9,
+        //     nombre: 'Federico Girotti',
+        //     descripcion: 'Federico Girotti es un futbolista profesional argentino que se desempeña en la posición de delantero y actualmente juega en el Club Atlético Talleres de la Primera División de Argentina',
+        //     imagen: "girotti.png",
+        //     detalle: 'Talleres - Delantero - 02/03/1999 - ARG',
+        //     comentarios: [
+        //         {
+        //             id: 1,
+        //             usuario: "Julian",
+        //             texto:"Recien comenzando. Crack total",
+        //         },
+        //     ]
+        // },
+
+        {
+            id:10,
+            nombre: 'German Salort',
+            descripcion: 'Germán Guillermo Salort es un futbolista argentino. Juega de Arquero y su equipo actual es Comunicaciones, de la Primera Nacional',
+            imagen: "German salort.jpg",
+            detalle: 'Comunicaciones - Arquero - 22/2/2000 - ARG',
+            comentarios: [
+                {
+                    id: 1,
+                    usuario: "Julian",
+                    texto:"Recien comenzando. Crack total",
+                },
+            ]
+        },
+        {
+            id:11,
+            nombre: 'Leandro Brey',
+            descripcion: 'Leandro Brey es un futbolista argentino. Actualmente es el arquero de la Reserva del C.A Boca Juniors. Debutó con 17 años en Los Andes',
+            imagen: "brey_b2a82.jpg",
+            detalle: 'Boca Juniors - Arquero - 02/10/2001 - ARG',
+            comentarios: [
+                {
+                    id: 1,
+                    usuario: "Miguel",
+                    texto:"Recien comenzando. Crack total",
+                },
+            ]
+        },
+        {
+            id:12,
+            nombre: 'Augusto Batalla',
+            descripcion: 'Augusto Batalla es un futbolista argentino. Actualmente es el arquero titular de San Lorenzo de Almagro. Es de la cantera de River y estuvo al borde de jugar en el Real Madrid',
+            imagen: "batalla.jpg",
+            detalle: 'San Lorenzo - Arquero - 13/04/1997 - ARG',
+            comentarios: [
+                {
+                    id: 1,
+                    usuario: "Roni",
+                    texto:"Recien comenzando. Crack total",
+                },
+            ]
+        }
+
+    ],
+    volantes: [
         {
             id:2,
             nombre: 'Baltasar Barcia',
@@ -54,7 +219,61 @@ let productos = {
                 },
             ]
         },
+        {
+                id:5,
+                nombre: 'Vicente Taborda',
+                descripcion: 'Lisandro López es un futbolista] argentino que se desempeña como delantero en el Club Atlético Sarmiento de la Primera División de Argentina.',
+                imagen: "taborda.jpeg",
+                detalle: 'Platense - Volante Ofensivo - 22/05/2001 - ARG',
+                comentarios: [
+                    {
+                        id: 1,
+                        usuario: "Julian",
+                        texto:"JUgador joven crack",
+                    },
+                ]
+            },
+        {
+                id:15,
+                nombre: 'Cristian Medina',
+                descripcion: 'Cristian Medina es un futbolista] argentino que se desempeña como delantero en el Club Atlético Boca Jrs. de la Primera División de Argentina.',
+                imagen: "medina.jpeg",
+                detalle: 'Boca Juniors - Volante - 12/12/2001 - ARG',
+                comentarios: [
+                    {
+                        id: 1,
+                        usuario: "Julian",
+                        texto:"JUgador joven crack",
+                    },
+                ]
+            }
+    ],
+    delanteros: [
+        {
+            id:1,
+            nombre: 'Darío Benedetto',
+            descripcion: 'Darío Ismael "Pipa" Benedetto es un futbolista argentino que juega de Delantero como número 9. Actualmente es propiedad del Club Atlético Boca Juniors de la Liga Profesional de Fútbol Argentino.',
+            imagen: "benedetto_84f59.jpg",
+            detalle: 'Boca Juniors - Delantero Centro - 17/05/1990 - ARG',
+            comentarios: [
+                {
+                    id: 1,
+                    usuario: "Sebastián Battaglia",
+                    texto:"Excelente jugador el Pipa! Muchisima experiencia y potrero. Lo recomiendo para cualquier club, gran 9",
+                },
+               {
+                    id: 2,
+                    usuario: "Gustavo Alfaro",
+                    texto:"Un gran referente de plantel y excelente profesional, conmigo siempre se portó de maravilla y respeto" 
+                }, 
+                {
+                    id: 3,
+                    usuario: "Guillermo Barros Schelotto",
+                    texto:"Tiene un olfato goleador marcado, cuando lo dirigí en 2017 me hizo acordar mucho al mejor Palermo. Es el nuevo ídolo de Boca" 
+                }
 
+            ]
+        },
         {
             id:4,
             nombre: 'Lisandro Lopez',
@@ -69,38 +288,6 @@ let productos = {
                 },
             ]
         },
-
-        {
-            id:5,
-            nombre: 'Vicente Taborda',
-            descripcion: 'Lisandro López es un futbolista] argentino que se desempeña como delantero en el Club Atlético Sarmiento de la Primera División de Argentina.',
-            imagen: "taborda.jpeg",
-            detalle: 'Platense - Volante Ofensivo - 22/05/2001 - ARG',
-            comentarios: [
-                {
-                    id: 1,
-                    usuario: "Julian",
-                    texto:"JUgador joven crack",
-                },
-            ]
-        },
-
-        
-        {
-            id:6,
-            nombre: 'Gabriel Arias',
-            descripcion: 'Gabriel Arias Arroyo es un futbolista chileno nacido en Argentina. Juega como arquero en Racing Club de la Primera División de Argentina y es internacional con la selección de Chile.',
-            imagen: "880_arias.jpg",
-            detalle: 'Racing - Arquero - 02/03/1990 - CHI',
-            comentarios: [
-                {
-                    id: 1,
-                    usuario: "Julian",
-                    texto:"Idolo en Racing. Las gano todas",
-                },
-            ]
-        },
-
         {
             id:7,
             nombre: 'Mateo Retegui',
@@ -135,7 +322,7 @@ let productos = {
             nombre: 'Federico Girotti',
             descripcion: 'Federico Girotti es un futbolista profesional argentino que se desempeña en la posición de delantero y actualmente juega en el Club Atlético Talleres de la Primera División de Argentina',
             imagen: "girotti.png",
-            detalle: 'Talleres - Delantero Centro - 02/03/1999 - ARG',
+            detalle: 'Talleres - Delantero - 02/03/1999 - ARG',
             comentarios: [
                 {
                     id: 1,
@@ -144,25 +331,6 @@ let productos = {
                 },
             ]
         },
-
-        {
-            id:10,
-            nombre: 'German Salort',
-            descripcion: 'Germán Guillermo Salort es un futbolista argentino. Juega de Arquero y su equipo actual es Comunicaciones, de la Primera Nacional',
-            imagen: "German salort.jpg",
-            detalle: 'Comunicaciones - Arquero - 01/03/1999 - ARG',
-            comentarios: [
-                {
-                    id: 1,
-                    usuario: "Julian",
-                    texto:"Recien comenzando. Crack total",
-                },
-            ]
-        },
-        
-        
-        
-
     ]
 }
 module.exports= productos;
