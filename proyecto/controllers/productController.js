53fff41b9fa71a026854b451addf6a13980607f4
Manuelzimmermann <zimmermannm@udesa.edu.ar>
2023-04-10 12:04:14 -0300

    cambios

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -9,6 +9,16 @@ let productController = {
     add: function (req, res){
         return res.render('product-add')
     },
+    product: function (req, res){
+        let lista = []
+        let info = dbA.listaa
+        for (let i =0; i< info.length; i++){
+            if (info[i].id == req.params.id){
+                lista.push(info[i])
+            }
+        }
+        return res.render('product', {nombre: lista[0].nombre, descripcion: lista[0].descripcion, imagen: lista[0].imagen})
+    }, 
     show: function (req, res){
         return res.render('product', {producto: db.delanteros})
         
