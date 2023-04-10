4fd3cefda9eec3cb207e3db93abb4e3494598b1b
arielbercovich <abercovich@udesa.edu.ar>
2023-04-10 11:13:30 -0300

    increible

--- proyecto/db/usuario.js
+++ proyecto/db/usuario.js
@@ -1,11 +1,11 @@
 let usuario = [
     {
         email:"matias12@gmail.com",
-        usuario: "Matias Fernandez",  
+        nombre: "Matías Fernández",  
         contraseña: "Maticapo12",
         fechaDeNacimiento: "16/9/2001",
-        numeroDeDocumento: "44937403",
-        fotoDePerfil: "default-image.png"
+        dni: "44937403",
+        foto: "/images/users/perfil.png"
     }
     
 ];
