bbc8e4462b9d607e466cf4e716e408d74ec5f365
Manuel <zimmermannm@udesa.edu.ar>
2023-05-29 12:18:33 -0300

    clase

--- proyecto/controllers/loginController.js
+++ proyecto/controllers/loginController.js
@@ -2,8 +2,27 @@ let db = require('../db/usuario.js')
 let fav = require('../db/productos.js');
 let loginController = {
     index: function(req, res){
-        return res.render('login')
+        // si el user esta logueado, redirigirlo a home
+        if (req.session.user != undefined){
+            return res.redirect ('/')
+        } 
+        else{
+            return res.render('login')
+        }
+        // return res.render('login')
     },
+    // processLogin: function (req, res){
+        // tengo qur buscsar los datos de la db 
+        //ponerlos en la session
+
+
+        // return res.send(req.session){
+            email: 'traer de base de datos',
+            userName: 'traer de base de datos'
+        }
+        //y si el usuario quiere, agregar la cookie para que lo recuerde
+
+    // },
     profile: function(req,res){
         return res.render('profile', {nombre: db[0].nombre, email: db[0].email, clave: db[0].contraseña, cumple: db[0].fechaDeNacimiento, dni: db[0].dni, foto: db[0].foto, defensores: fav.defensores})
     },
