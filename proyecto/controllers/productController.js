aeafb692ded4cdd275fdc7169ed74463b6ebf07a
Manuel <zimmermannm@udesa.edu.ar>
2023-06-07 11:28:51 -0300

    producto

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -21,19 +21,19 @@ let productController = {
             include: [{association: 'comentario'}, {association:'usuario'}]
         })
         .then(function(producto){
-            return res.render("product", producto)     
+            return res.render("product", {producto: producto})     
             console.log(producto)
-            // return res.render('product', producto);
-            // let comentadores = [];
-            //     for(let i =0; i<producto.comentario.length; i++){
-            //     modelos.Usuario.findByPk(producto.comentario[i].id_usuario)
-            //     .then(function(unComentador){
-            //         comentadores.push(unComentador)
-            //         if (i == producto.comentario.length - 1){
-            //             res.render('product', {nombre: producto.nombre_producto, descripcion: producto.descripcion, imagen: producto.foto, comentarios: producto.comentario, usuario: producto.usuario, comentadores: comentadores})
-            //         }
-            //     })
-            // }
+            return res.render('product', producto);
+            let comentadores = [];
+                for(let i =0; i<producto.comentario.length; i++){
+                modelos.Usuario.findByPk(producto.comentario[i].id_usuario)
+                .then(function(unComentador){
+                    comentadores.push(unComentador)
+                    if (i == producto.comentario.length - 1){
+                        res.render('product', {nombre: producto.nombre_producto, descripcion: producto.descripcion, imagen: producto.foto, comentarios: producto.comentario, usuario: producto.usuario, comentadores: comentadores})
+                    }
+                })
+            }
 
             })
     
